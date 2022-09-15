0f6fa3438813bbb6ae89549ccb346f4d7b7fc1f3
chelmi00 <chelmi00@github.com>
2022-09-15 17:39:37 +0200

    #4

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+function Welcome(props) {
+  return (
+    <h1>Hello {props.user}!</h1>
+  );
+}
+
+class Welcome2 extends React.Component {
+  render() {
+    return (
+      <h1>Hello {this.props.user}!</h1>
+    );
+  }
+}
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <Welcome user="Bob" />
+      <Welcome user="Miguel" />
+    </div>
   );
 }
 
