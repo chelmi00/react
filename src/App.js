a45e5629cef7b283db926b7384e997996de5d3e2
chelmi00 <chelmi00@github.com>
2022-09-16 10:10:23 +0200

    #8 v2

--- src/App.js
+++ src/App.js
@@ -3,15 +3,13 @@ import React from "react";
 export default function App() {
 
   let isLoggedIn = false;
-  const RenderButton = () => {
-    if (isLoggedIn) {
-      return <button>Logout</button>;
-    } else {
-      return <button>Login</button>;
-    }
-  };
 
   return (
-    <RenderButton />
-  );
+    (function() {
+      if (isLoggedIn) {
+        return <button>Logout</button>;
+      } else {
+        return <button>Login</button>;
+      }
+    }) ());
 }
